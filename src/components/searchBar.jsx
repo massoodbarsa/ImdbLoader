e18ffe7086fcae8a79ab6e029724214791f10638
Amir Shakiba Nasab <amir.shakiba1980@gmail.com>
2020-08-01 03:56:23 +0200

    add enter key to search,searchBar component to function

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faFileDownload } from "@fortawesome/free-solid-svg-icons";
 import { Input, Button } from "@material-ui/core";
@@ -7,63 +7,61 @@ import { connect } from "react-redux";
 import { fetchMovieFromApi } from "../Store/actions/moviesAction";
 import { Link } from "react-router-dom";
 
+function searchBar(props) {
+  const [search, setSearch] = useState({
+    searchItem: "",
+  });
 
-class searchBar extends Component {
-  state = {
-    search: "",
-  };
-
-  handleChange = (e) => {
+  const handleChange = (e) => {
     e.preventDefault();
-    this.setState({
-      search: e.target.value,
+    setSearch({
+      searchItem: e.target.value,
     });
   };
 
-  handleClick = () => {
-    this.props.fetchMovieFromApi(this.state.search);
+  const handleClick = () => {
+    props.fetchMovieFromApi(search.searchItem);
   };
 
-  pageContentLength = () => {
-    const valiedMovies = this.props.movies
-      ? this.props.movies.filter((item) => item.title !== undefined)
+  const onKeyPressed = (e) => {
+    e.key === "Enter" ? props.fetchMovieFromApi(search.searchItem) : null;
+  };
+
+  const pageContentLength = () => {
+    const valiedMovies = props.movies
+      ? props.movies.filter((item) => item.title !== undefined)
       : [];
 
     return valiedMovies.length;
   };
 
-  render() {
-    return (
-      <div>
-        <section className="search-area">
-          <label>Search</label>
-          <Input
-            type="text"
-            className="search-input"
-            onChange={this.handleChange}
-            color="primary"
-          />
-          <a
-            className="btn-floating btn-large waves-effect waves-light cyan lighten-2 "
-            onClick={this.handleClick}
-          >
-            <Button color="primary">
-              <FontAwesomeIcon icon={faSearch} size="2x" color="#286b92" />
-            </Button>
-          </a>
-          <Link to="/">
-            <Badge color="secondary" badgeContent={this.pageContentLength()}>
-              <FontAwesomeIcon
-                icon={faFileDownload}
-                size="2x"
-                color="#286b92"
-              />
-            </Badge>
-          </Link>
-        </section>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <section className="search-area">
+        <label>Search</label>
+        <Input
+          type="text"
+          className="search-input"
+          onChange={handleChange}
+          color="primary"
+          onKeyDown={(e) => onKeyPressed(e)}
+        />
+        <a
+          className="btn-floating btn-large waves-effect waves-light cyan lighten-2 "
+          onClick={handleClick}
+        >
+          <Button color="primary">
+            <FontAwesomeIcon icon={faSearch} size="2x" color="#286b92" />
+          </Button>
+        </a>
+        <Link to="/">
+          <Badge color="secondary" badgeContent={pageContentLength()}>
+            <FontAwesomeIcon icon={faFileDownload} size="2x" color="#286b92" />
+          </Badge>
+        </Link>
+      </section>
+    </div>
+  );
 }
 const mapStateToProps = (state) => {
   return { movies: state.movie.movies, loading: state.movie.loading };
